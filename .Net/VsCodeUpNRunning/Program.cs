a5d0b1531d9637346f2f39e78c11560b477164f1
owen <owen_0918@live.com>
2020-03-29 12:29:55 +0600

    first commit ...

---
+++ .Net/VsCodeUpNRunning/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VsCodeUpNRunning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(GCD(15, 35));
+        }
+
+        public static int GCD(int a, int b){
+            while(b !=0) {
+                int remainder = a % b;
+
+                a = b;
+                b = remainder;
+            }
+            return a;
+        }
+    }
+}
